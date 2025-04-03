63d074bddbb35e8fcc08b0a1a523ae00d9d1fa19
BaldursGoonsack <149293305+BaldursGoonsack@users.noreply.github.com>
2025-04-03 13:28:35 +1100

    lua table

--- Mods/Ultimate_NPC_Stat_Overhaul/ScriptExtender/Lua/BootstrapServer.lua
+++ Mods/Ultimate_NPC_Stat_Overhaul/ScriptExtender/Lua/BootstrapServer.lua
@@ -1,6 +1,16 @@
-Ext.Require("Server/Classes/_Barbarian.lua")
-Ext.Require("Server/Classes/_Barbarian.lua")
-
-local ModTable = "Ultimate_NPC_Stat_Overhaul" -- Replace with actual mod identifier
+local ModTable = "Ultimate_NPC_Stat_Overhaul"
 Mods[ModTable] = Mods[ModTable] or {}
 Mods[ModTable].PersistentVars = Mods[ModTable].PersistentVars or {}
+
+Ext.Require("Server/Classes/_Barbarian.lua")
+Ext.Require("Server/Classes/_Bard.lua")
+Ext.Require("Server/Classes/_Cleric.lua")
+Ext.Require("Server/Classes/_Druid.lua")
+Ext.Require("Server/Classes/_Fighter.lua")
+Ext.Require("Server/Classes/_Monk.lua")
+Ext.Require("Server/Classes/_Paladin.lua")
+Ext.Require("Server/Classes/_Ranger.lua")
+Ext.Require("Server/Classes/_Rogue.lua")
+Ext.Require("Server/Classes/_Sorcerer.lua")
+Ext.Require("Server/Classes/_Warlock.lua")
+Ext.Require("Server/Classes/_Wizard.lua")
