6dfc19409cac11c6c0c1d008923c30c45b1e04fb
osirisofgit <github.com.j95fw@passmail.net>
2025-04-25 12:57:13 -0500

    start functors, deal with later

---
+++ Ultimate_NPC_Stat_Overhaul/Mods/Ultimate_NPC_Stat_Overhaul/ScriptExtender/Lua/Client/Stats/Proxies/Functors.lua
@@ -0,0 +1,21 @@
+FunctorsProxy = StatProxy:new()
+
+FunctorsProxy.statsToParse = {
+}
+
+StatProxy:RegisterStatType("StatsFunctors", FunctorsProxy)
+
+---@param functors StatsFunctors
+function FunctorsProxy:buildHyperlinkedStrings(parent, functors)
+	if functors and functors ~= "" then
+		for _, functor in ipairs(functors.FunctorList) do
+			if functor.TypeId == "ApplyStatus" then
+				local functorString = string.format("%s(%s, %s,%s,%s,%s)")
+
+			elseif functor.TypeId == "RemoveStatus" then
+			else
+				parent:AddText(string.format("TypeId: %s, NOT MAPPED"), tostring(functor.TypeId))
+			end
+		end
+	end
+end
