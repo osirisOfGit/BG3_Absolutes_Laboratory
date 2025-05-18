94114902a14035ed1c80107297642c72525a03f0
osirisofgit <github.com.j95fw@passmail.net>
2025-05-18 14:51:18 -0500

    game level

--- Absolutes_Laboratory/Mods/Absolutes_Laboratory/ScriptExtender/Lua/Shared/Mutations/Mutators/HealthMutator.lua
+++ Absolutes_Laboratory/Mods/Absolutes_Laboratory/ScriptExtender/Lua/Shared/Mutations/Mutators/HealthMutator.lua
@@ -33,7 +33,7 @@ function HealthMutator:renderModifiers(parent, modifiers)
 	--#region Character Level
 	---@type HealthClassLevelModifier
 	local characterLevelModifier = modifiers["CharacterLevel"] or {
-		value = 1,
+		value = 0,
 		extraData = {}
 	} --[[@as HealthClassLevelModifier]]
 
@@ -41,19 +41,19 @@ function HealthMutator:renderModifiers(parent, modifiers)
 
 	characterLevelModifier.extraData = characterLevelModifier.extraData or {}
 
-	parent:AddText("Each level adds")
+	local cLevelInfoText = parent:AddSeparatorText("Character Level Modifiers ( ? )")
+	cLevelInfoText:SetStyle("SeparatorTextAlign", 0, 0.3)
+	cLevelInfoText:SetStyle("Alpha", 1)
+	cLevelInfoText:Tooltip():AddText(
+		"\t Set the levels at which the modifier changes - for example, setting the modifier to 10% at level 5\nwhen the base modifier is 5% means the modifier will be 5% levels 1-4 and 10% levels 5+")
+
+	parent:AddText("Each character level adds")
 	local baseCLevelMod = parent:AddInputInt("% to the % Base Health Mutator##characterLevel", characterLevelModifier.value)
 	baseCLevelMod.ItemWidth = 40
 	baseCLevelMod.SameLine = true
 	baseCLevelMod.OnChange = function()
 		characterLevelModifier.value = baseCLevelMod.Value[1]
 	end
-
-	local cLevelInfoText = parent:AddSeparatorText("Character Level Modifiers ( ? )")
-	cLevelInfoText:SetStyle("SeparatorTextAlign", 0, 0.3)
-	cLevelInfoText:SetStyle("Alpha", 1)
-	cLevelInfoText:Tooltip():AddText(
-		"Set the levels at which the modifier changes - for example, setting the modifier to 10% at level 5\nwhen the base modifier is 5% means the modifier will be 5% levels 1-4 and 10% levels 5+")
 
 	local cLevelTable = parent:AddTable("characterModifierCustomizer", 2)
 	local headers = cLevelTable:AddRow()
@@ -92,4 +92,52 @@ function HealthMutator:renderModifiers(parent, modifiers)
 	end
 
 	--#endregion
+
+	--#region Game Level
+	---@type HealthClassLevelModifier
+	local gameLevelModifier = modifiers["GameLevel"] or {
+		value = 0,
+		extraData = {}
+	} --[[@as HealthClassLevelModifier]]
+
+	modifiers["GameLevel"] = gameLevelModifier
+
+	gameLevelModifier.extraData = gameLevelModifier.extraData or {}
+
+	local gLevelInfoText = parent:AddSeparatorText("Game Level Modifiers ( ? )")
+	gLevelInfoText:SetStyle("SeparatorTextAlign", 0, 0.3)
+	gLevelInfoText:SetStyle("Alpha", 1)
+	gLevelInfoText:Tooltip():AddText(
+		"\t Set the levels at which the modifier changes - for example, setting the modifier to 10% for SCL_MAIN_A\nwhen the base modifier is 5% means the modifier will be 5% on TUT, WLD, and SCL and 10% after. Setting to 0 will just use base.")
+
+	parent:AddText("Each game level adds")
+	local baseGLevelMod = parent:AddInputInt("% to the % Base Health Mutator##gameLevel", gameLevelModifier.value)
+	baseGLevelMod.ItemWidth = 40
+	baseGLevelMod.SameLine = true
+	baseGLevelMod.OnChange = function()
+		gameLevelModifier.value = baseGLevelMod.Value[1]
+	end
+
+	local gLevelTable = parent:AddTable("gameModifierCustomizer", 2)
+	local gameHeaders = gLevelTable:AddRow()
+	gameHeaders.Headers = true
+	gameHeaders:AddCell():AddText("Level")
+	gameHeaders:AddCell():AddText("% Modifier")
+
+	for _, level in ipairs(EntityRecorder.Levels) do
+		local row = gLevelTable:AddRow()
+		local levelCell = row:AddCell()
+
+		gameLevelModifier.extraData[level] = gameLevelModifier.extraData[level] or 0
+
+		levelCell:AddText(level)
+
+		local modInput = row:AddCell():AddInputInt("##" .. level, gameLevelModifier.extraData[level])
+
+		modInput.OnDeactivate = function()
+			gameLevelModifier.extraData[gameLevelModifier.extraData[level]] = modInput.Value[1]
+		end
+	end
+	--#endregion
+	
 end
